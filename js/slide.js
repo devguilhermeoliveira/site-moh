ca2b9d456ce6f883bd77d2f7f798103055679a36
Guilherme Oliveira <126362334+devguilhermeoliveira@users.noreply.github.com>
2023-05-31 18:22:54 -0300

    up

--- js/slide.js
+++ js/slide.js
@@ -33,3 +33,16 @@
   ativarSlide()
 
 }())
+
+var msgCookies = document.getElementById('cookies-msg')
+
+function aceito(){
+    localStorage.lgpd = "sim"
+    msgCookies.classList.remove('mostrar')
+}
+
+if(localStorage.lgpd == 'sim'){
+     msgCookies.classList.remove('mostrar')
+}else{
+    msgCookies.classList.add('mostrar')
+}

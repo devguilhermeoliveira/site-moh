06f5ecca74be0254d8476bbe06b7f92b27bad772
Guilherme Oliveira <126362334+devguilhermeoliveira@users.noreply.github.com>
2023-05-31 18:10:31 -0300

    update

--- js/bloquear-arrasta.js
+++ js/bloquear-arrasta.js
@@ -10,3 +10,16 @@
   desativarDragDrop()
 
 }())
+
+var msgCookies = document.getElementById('cookies-msg')
+
+function aceito(){
+    localStorage.lgpd = "sim"
+    msgCookies.classList.remove('mostrar')
+}
+
+if(localStorage.lgpd == 'sim'){
+     msgCookies.classList.remove('mostrar')
+}else{
+    msgCookies.classList.add('mostrar')
+}
